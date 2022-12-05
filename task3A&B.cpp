197d384a4de12e0e893fb49e6eb15961efd4b65e
Aigulzinnatovaa <95309447+Aigulzinnatovaa@users.noreply.github.com>
2022-12-05 14:25:26 +0300

    Create task3A&B.cpp

---
+++ task3A&B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    std::cout << "Введите a: ";
+    std::cin >> a;
+    printf( "%d  %x", a,a);
+    
+    int b;
+    std::cout << "\n Введите b: ";
+    std::cin >> b;
+    printf( "%d  %x", b,b);
+
+}
